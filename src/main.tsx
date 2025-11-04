7421c7ce4ebe13597ad9cf1bb768cd39df7a8449
Nejdan4k <a.stryzhavchuk@gmail.com>
2025-11-03 22:56:20 -0500

    fixed2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
-import './App.css'; // <— ВАЖЛИВО: точна назва файлу з тим самим регістром
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './components/App/App';
